0ee65307c5df6544a930db2e7f89bb526afcda9b
yaseen <yaseenaniss@gmail.com>
2020-08-30 21:13:16 -0700

    removing config

--- app/config.py
+++ app/config.py
@@ -5,13 +5,3 @@ basedir = path.abspath(path.dirname(__file__))
 
 class Config:
     """Set Flask configuration from .env file."""
-
-    # General Config
-    SECRET_KEY = environ.get('POKER_SECRET_KEY')
-    FLASK_APP = environ.get('POKER_FLASK_APP')
-    FLASK_ENV = environ.get('POKER_FLASK_ENV')
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
