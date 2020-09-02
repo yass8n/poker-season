c353021642fd8fb901998f0317b8fc9c4df45f2d
yaseen <yaseenaniss@gmail.com>
2020-09-02 04:41:32 -0700

    added leaderboard and games view to landing page

--- app/main.py
+++ app/main.py
@@ -3,14 +3,27 @@ import logging
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
 from app.db import db_fetch
-from app.poker_db import get_season_results
+from app.poker_db import get_season_results, get_game_results_for_season
+from app.helpers import make_ordinal
 
 @app.route('/')
 def index():
-    data = get_season_results(season_id=1)
+    season_id = 1
+    leaderboard_results = get_season_results(season_id=season_id)
+    game_results = get_game_results_for_season(season_id=season_id)
+    hash = {}
+    current_placement = 0
+    for leaderboard_result in leaderboard_results:
+        if leaderboard_result['total_points'] not in hash.keys():
+            current_placement = current_placement + 1
+            leaderboard_result['placement'] = make_ordinal(current_placement)
+            hash[leaderboard_result['total_points']] = True
+        else:
+            leaderboard_result['placement'] = make_ordinal(current_placement)
     all_data = {
-        "data" : data,
-        "season_number" : 2
+        "leaderboard_results" : leaderboard_results,
+        "game_results" : game_results,
+        "season_number" : season_id
     }
     return render_template('index.html', **all_data)
 
--- app/poker_db.py
+++ app/poker_db.py
@@ -12,7 +12,7 @@ CASE
     WHEN placement_points.placement = 2 THEN '2nd'
     WHEN placement_points.placement = 3 THEN '3rd'
     ELSE CONCAT(placement_points.placement, 'th')
-END
+END SEPARATOR ', '
 ) as placements
 FROM games JOIN seasons on games.season_id = seasons.id
 JOIN games_placements on games_placements.game_id = games.id
@@ -21,6 +21,46 @@ JOIN placement_points on placement_points.player_count = games.player_count AND 
 WHERE seasons.id = {season_id}
 GROUP BY players.id
 ORDER BY total_points DESC"""
+    return db_fetch(sql)
+
+def get_game_results_for_season(season_id: int):
+    sql = f"""
+   (
+SELECT
+game_number,
+games.player_count as total_players,
+CASE
+    WHEN placement_points.placement = 1 THEN "1st"
+    WHEN placement_points.placement = 2 THEN "2nd"
+    WHEN placement_points.placement = 3 THEN "3rd"
+    ELSE CONCAT(placement_points.placement, "th")
+END as placement,
+players.username,
+placement_points.points as points_earned
+FROM games JOIN seasons on games.season_id = seasons.id
+JOIN games_placements on games_placements.game_id = games.id
+JOIN players on players.id = games_placements.player_id
+JOIN placement_points on placement_points.player_count = games.player_count AND games_placements.placement = placement_points.placement
+WHERE seasons.id = {season_id}
+ORDER BY game_number DESC, points_earned DESC
+)
+
+UNION
+
+(
+SELECT
+game_number,
+games.player_count as total_players,
+NULL as placement,
+players.username,
+0 as points_earned
+FROM games JOIN seasons on games.season_id = seasons.id
+JOIN games_placements on games_placements.game_id = games.id
+JOIN players on players.id = games_placements.player_id
+WHERE games_placements.placement IS NULL AND seasons.id = {season_id}
+ORDER BY game_number DESC, points_earned DESC
+)
+ORDER BY game_number DESC, points_earned DESC"""
     return db_fetch(sql)
 
 def populate_game_results(usernames_and_placements: dict, season_id: int, game_number: int):
@@ -95,7 +135,6 @@ AND games.season_id = {season_id}
 
 # CREATE VIEW game_results_view AS
 # (
-# (
 # SELECT
 # game_number,
 # games.player_count as total_players,
@@ -131,8 +170,7 @@ AND games.season_id = {season_id}
 # ORDER BY game_number DESC, points_earned DESC
 # )
 # ORDER BY game_number DESC, points_earned DESC
-#
-# )
+
 
 
 #
