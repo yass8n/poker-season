040db7831c7ebe62e3661e3eb928debfafb3beef
yaseen <yaseenaniss@gmail.com>
2020-09-06 02:26:16 -0700

    done with season/club relationship. Nav bar dropdown works properly

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template
-import logging
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
-from app.db import db_fetch
-from app.poker_db import get_season_results, get_game_results_for_season
+from app.poker_db import get_season_results, get_game_results_for_season, get_all_seasons
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
-@app.route('/seasons/<season_id>')
-def show(season_id: int):
+@app.route('/clubs/<club_id>/seasons/<season_id>')
+def show(club_id: int, season_id: int):
     season_leaderboard_results = get_season_results(season_id=season_id)
     game_results = get_game_results_for_season(season_id=season_id)
-
+    club_seasons = get_all_seasons(club_id)
     all_data = {
+        "club_seasons" : club_seasons,
         "season_leaderboard_results" : season_leaderboard_results,
         "game_results" : game_results,
         "season_number" : season_id
