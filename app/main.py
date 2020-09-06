bb52568e5c2aa0e953fc5317c12396823822e4a7
yaseen <yaseenaniss@gmail.com>
2020-09-06 02:59:17 -0700

    removed some unneeded code and changed route copy

--- app/main.py
+++ app/main.py
@@ -7,16 +7,18 @@ from app.models import Season, Club
 def index():
     return render_template('base.html')
 
-@app.route('/clubs/<club_id>/seasons/<season_id>')
+@app.route('/club/<club_id>/season/<season_id>')
 def show(club_id: int, season_id: int):
     season_leaderboard_results = Season.get_season_results(season_id=season_id)
     game_results = Season.get_game_results_for_season(season_id=season_id)
+    club = Club.query.get(club_id)
     club_seasons = Club.get_all_seasons(club_id=club_id)
     all_data = {
         "club_seasons" : club_seasons,
         "season_leaderboard_results" : season_leaderboard_results,
         "game_results" : game_results,
-        "season_number" : season_id
+        "season_number" : season_id,
+        "club" : club
     }
     return render_template('index.html', **all_data)
 
